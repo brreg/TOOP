59c05975d705a609a3a84c68246eaa6a2c01ba36
Frode Roxrud Gill <extfrg@brreg.no>
2020-08-18 15:17:14 +0200

    Added TOOP Connector initialize and shutdown

--- src/main/java/no/brreg/toop/Application.java
+++ src/main/java/no/brreg/toop/Application.java
@@ -1,11 +1,19 @@
 package no.brreg.toop;
 
+import com.helger.web.scope.mgr.WebScopeManager;
+import eu.toop.connector.app.TCInit;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.annotation.PreDestroy;
+import javax.servlet.ServletContext;
 
 
 @SpringBootApplication
@@ -18,6 +26,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     private static Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
+    @Autowired
+    private ServletContext servletContext;
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initializeToopConnector() {
+        WebScopeManager.onGlobalBegin(servletContext);
+        TCInit.initGlobally(servletContext);
+    }
+
+    @PreDestroy
+    public void shutdownToopConnector() {
+        TCInit.shutdownGlobally(servletContext);
+        WebScopeManager.onGlobalEnd();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
