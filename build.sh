d45466e3d121e6702af825586b57296278f48db1
extfrg <extfrg@brreg.no>
2020-09-14 22:09:01 +0200

    Added build script

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+oc login https://api.ocp-prd.regsys.brreg.no:6443 &&
+oc project toop &&
+mvn clean install &&
+docker push quay.apps.ocp-svc.base.brreg.no/toop/toop-smp:latest &&
+docker push quay.apps.ocp-svc.base.brreg.no/toop/toop-connector:latest &&
+oc import-image toop-smp &&
+oc import-image toop-connector
